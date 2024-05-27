d07aa35d9d0d1b9cb15fa958599ac5fcb5ea430e
Arno Schutijzer <arno.schutijzer@protonmail.com>
2024-05-27 18:34:34 +0200

    switch exporter based on flag

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/algleymi/certificate-manager/internal/exporters"
@@ -8,21 +11,25 @@ import (
 )
 
 func main() {
-	o, err := onepassword.NewOnePassword()
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "%s <format>\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	format := flag.String("format", "text", "switch between \"text\" and \"csv\"")
+	flag.Parse()
 
+	o, err := onepassword.NewOnePassword()
 	if err != nil {
 		panic(err)
 	}
 
 	inAMonth := time.Now().AddDate(0, 1, 0)
-
 	outdatedCertificates, err := o.FindCertificatesOlder(inAMonth)
-
 	if err != nil {
 		panic(err)
 	}
 
-	exporter := &exporters.ConsoleExporter{}
+	exporter := exporters.CreateExporter(*format)
 	err = exporter.Export(outdatedCertificates)
 
 	if err != nil {
--- internal/exporters/exporter.go
+++ internal/exporters/exporter.go
@@ -5,3 +5,11 @@ import "github.com/algleymi/certificate-manager/internal"
 type Exporter interface {
 	Export(certificates []internal.Certificate) error
 }
+
+func CreateExporter(format string) Exporter {
+	if format == "csv" {
+		return &CsvExporter{}
+	}
+
+	return &ConsoleExporter{}
+}
